69cfe4aca64f142e3df83d95e6e974402ff59959
Florencia Amendola <amendolaflorencia@gmail.com>
2021-09-14 18:22:54 -0300

    buttons functions

---
+++ app.js
@@ -0,0 +1,12 @@
+let direcciones = ["Av. Almirante Brown 1200", "Av. Juan B. Alberdi 6101", ]
+
+/////////      FUNCION PARA DAR LA DIRECCION ONCLICK BOTON    ////////////
+function darDireccion(direccion) {
+	alert(direccion);
+};
+
+
+/////////      FUNCION PARA HACER PEDIDO ONCLICK BOTON     ////////////
+function hacerPedido() {
+	window.prompt("Describa su Pedido")	
+}
